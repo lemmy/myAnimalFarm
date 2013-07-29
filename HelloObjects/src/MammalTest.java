7ccb8d66450f5da50bfc288639f22da1829fe492
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-07-29 11:18:20 +0200

    25b - JUnit test case to verfiy that Dogs do and Sheeps do not accept non-vegetarian CanOfDogFood composites (Introduces test driven development).

---
+++ HelloObjects/src/MammalTest.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MammalTest {
+
+	@Test
+	public void testFeedDog() {
+		CanOfDogFood aFood = new CanOfDogFood();
+		aFood.addIngredient(new Apple());
+		aFood.addIngredient(new Potato());
+		aFood.addIngredient(new Beef());
+
+		Mammal aMammal = new Dog("TestDog");
+		aMammal.feed(aFood);
+	}
+
+	@Test
+	public void testFeedSheep() {
+		CanOfDogFood aFood = new CanOfDogFood();
+		aFood.addIngredient(new Apple());
+		aFood.addIngredient(new Potato());
+		aFood.addIngredient(new Beef());
+
+		Mammal aMammal = new Sheep("TestSheep");
+		try {
+			aMammal.feed(aFood);
+		} catch (DisgustingException e) {
+			// Expected
+			return;
+		}
+		Assert.fail("Sheeps expected to throw DisgustingException when fed with meat!");
+	}
+}
